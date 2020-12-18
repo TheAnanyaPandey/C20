12ab1b80f6638a5034841c8a43575fdb7610ca54
TheAnanyaPandey <75836921+TheAnanyaPandey@users.noreply.github.com>
2020-12-18 16:46:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var movingrect, fixedrect;
+
+function setup() {
+  createCanvas(800,400);
+  movingrect = createSprite(400, 200, 50, 50);
+  movingrect.shapeColor = "green";
+
+  fixedrect = createSprite(600,200,50,50);
+  fixedrect.shapeColor = "green";
+}
+
+function draw() {
+  background(0);
+  movingrect.x = mouseX;
+  movingrect.y = mouseY;  
+
+  if(movingrect.x - fixedrect.x <= movingrect.width/2 + fixedrect.width/2 &&
+    fixedrect.x - movingrect.x <= fixedrect.width/2 + movingrect.width/2 &&
+    movingrect.y - fixedrect.y <= movingrect.height/2 + fixedrect.height/2 &&
+    fixedrect.y - movingrect.y <= fixedrect.height/2 + movingrect.height/2){
+    movingrect.shapeColor = "red";
+    fixedrect.shapeColor = "red";
+  }
+  
+  else {
+    movingrect.shapeColor = "green";
+    fixedrect.shapeColor = "green";
+  }
+  drawSprites();
+}
